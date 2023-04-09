ea1b94d5581834177b2531893ee92dbfcd0397b6
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2023-04-09 12:44:39 +0300

    36

---
+++ 3.cpp
@@ -0,0 +1,18 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main() {
+	
+	vector<int> mass(5);
+	for (int i = 0; i < 5; i++)
+	{
+	cin >> mass[i];
+	}
+	sort(mass.begin(), mass.end());
+	for (int i = 0; i < 5; i++)
+	{
+		cout <<  mass[i]<<"  ";
+	}
+}
